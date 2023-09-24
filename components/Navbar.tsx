20974f4b9fafcd88bb3f6f5e0d4139ed2cbfa7a0
AndreiDascalu <dascaluandrei172001@gmail.com>
2023-09-24 21:06:32 +0200

    Navbar and logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,14 +6,14 @@ import {GoogleLogin, GoogleLogout} from 'react-google-login'
 import {AiOutlineLogout} from 'react-icons/ai'
 import {BiSearch} from 'react-icons/bi'
 import {IoMdAdd} from 'react-icons/io'
-
+import Logo from 'utils/yapp-logo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      <Link>
-        <div>
-          <Image className='cursor-pointer' src ={}>
+    <div className='w-full flex justity-between items-center border-b-2 border-grey-200 py-2 px-4'>
+      <Link href={"/"}>
+        <div className='w-[100px] md:w-[130px]'>
+          <Image className='cursor-pointer' src ={Logo} alt='Yapp' layout = 'responsive'>
 
           </Image>
         </div>
