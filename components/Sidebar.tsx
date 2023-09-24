42ec7319fa2fc7a622639bc91c183845f88e0c35
AndreiDascalu <dascaluandrei172001@gmail.com>
2023-09-24 22:19:17 +0200

    Sidebar changes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NextPage} from 'next'
+import { useRouter } from 'next/dist/client/router'
+import Link from 'next/dist/client/link'
+import GoogleLogin from 'react-google-login'
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai'
+import {ImCancelCircle} from 'react-icons/im'
 
 const Sidebar = () => {
+  const [showSidebar, setshowSidebar] = useState(true)
+  const normalLink = 'flex items-center gap-3 hover p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#ed2647] hover:bg-gray-100'
+  const userProfile = false
+  
+
   return (
-    <div>Sidebar</div>
+    <div>
+      <div className='block xl:hidden m-2 ml-4 mt-3 text-xl'
+      onClick={() => setshowSidebar((prev) => !prev)}
+      >
+        {showSidebar ? <ImCancelCircle/> : <AiOutlineMenu/>}
+      </div>
+      {showSidebar && (
+        <div
+        className={`${
+          showSidebar ? 'xl:w-400' : 'w-20'
+        } flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3`}
+        >
+          <div className='xl:border-b-2 border-gray-200 xl:pb-4'>
+            <Link href="/">
+              <div className={normalLink}>
+                <p className='text-2xl'>
+                  <AiFillHome/>
+                </p>
+                <span className='text-xl hidden xl:block'>
+                  Recommended
+                </span>
+              </div>
+            </Link>
+          </div>
+          {!userProfile && (
+            <div className='px-2 py-4 hidden xl:block'>
+              <p className='text-gray-400'>
+                Log in to like and comment on videos
+              </p>
+            </div>
+          )}
+        </div>
+        
+      )}
+    
+    </div>
   )
 }
 
