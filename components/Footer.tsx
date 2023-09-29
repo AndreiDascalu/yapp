49e65494f09190f32e28a2674ca1d576511bbfc3
AndreiDascalu <dascaluandrei172001@gmail.com>
2023-09-29 15:34:21 +0200

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import  {footerList1, footerList2, footerList3} from '../utils/constants';
 
-function Footer() {
+const List = ({items, mt} : {items:  string[], mt:boolean}) => (
+  <div className= {`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+        {items.map((item) => (
+          <p key = {item} className='text-gray-400 text-sm hover:underline cursor-pointer'>
+            {item}
+          </p>
+        ))}
+      </div>  
+)
+
+const Footer= () => {
   return (
-    <div>Footer</div>
+    <div className='nt-6 hidden xl:block'>
+      <div className='xl:border-b-2 pb-6 xl:border-gray-200'>
+        <List items= {footerList1} mt={false}/>
+      </div>  
+      
+      <div className='xl:border-b-2 pb-6 xl:border-gray-200'>
+        <List items= {footerList2} mt ={true}/>
+      </div>  
+      
+      <div className='xl:border-b-2 pb-6 xl:border-gray-200'>
+        <List items= {footerList3} mt = {true}/>
+      </div>  
+
+    </div>
   )
 }
 
